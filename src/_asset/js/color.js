cdbc4a816fa30ec23cd157956be2f21a957ff4f2
AndreiSyntax <michia.andreivlad@gmail.com>
2019-03-07 12:16:32 +0000

    Color JS and content

---
+++ src/_asset/js/color.js
@@ -0,0 +1,48 @@
+let colorList {
+    'cascade': {
+    colorNormal: '#95A5A6',
+    colorDark: '#7F8C8D',
+  },
+  
+  'zest': {
+    colorNormal: '#E67E22',
+    colorDark: '#D35400',
+  },
+  
+  'buttercup': {
+    colorNormal: '#F1C40F',
+    colorDark: '#F39C12',
+  },
+  
+  'pickled-bluewood': {
+    colorNormal: '#34495E',
+    colorDark: '#2C3E50',
+  },
+  
+  'wisteria': {
+    colorNormal: '#9B59B6',
+    colorDark: '#8E44AD',
+  },
+  
+  'shamrock': {
+    colorNormal: '#2ECC71',
+    colorDark: '#27AE60',
+  },
+  
+  'mountain-meadow': {
+    colorNormal: '#1ABC9C',
+    colorDark: '#16A085',
+  },
+  
+  'curious-blue': {
+    colorNormal: '#3498db',
+    colorDark: '#2980b9',
+  },
+  
+  'cinnaba': {
+    colorNormal: '#e74c3c',
+    colorDark: '#c0392b',
+  },
+}
+
+console.log(colorList)
